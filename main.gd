92087f4612b6229f3f1f885b6b5e5850939525ae
Martin-Hayot <m.hayot@students.ephec.be>
2025-02-25 20:54:01 +0100

    feat: hud and attack state

--- main.gd
+++ main.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
-@onready var ui = $DayNightCycleUI
+@onready var ui = $CanvasLayer/DayNightCycleUI
+@onready var canvas_layer: CanvasLayer = $CanvasLayer
 @onready var canvas_modulate: CanvasModulate = $CanvasModulate
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	canvas_layer.visible = true  
 	canvas_modulate.time_tick.connect(ui.set_daytime)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
